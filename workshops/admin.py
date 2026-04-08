b4d6d846db7f8279ded31d35eae849eb0faf1a29
lbwright22ci <lbwright22@gmail.com>
2026-04-08 13:18:09 +0100

    start to build workshop type and workshop models in workshop app

--- workshops/admin.py
+++ workshops/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Workshop, WorkshopType
 
 # Register your models here.
+
+admin.site.register(WorkshopType)
+
+admin.site.register(Workshop)
+
--- workshops/models.py
+++ workshops/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class WorkshopType(models.Model):
+    
+    """ Stores a single workshop category (eg. kids, 1 hour workshop, costing £27.50 pp)"""
+
+    TYPE_CHOICES =(
+        ('CH', 'mixed ages 8-16 years'),
+        ('AD', 'adults & late teens'),
+        ('FA', 'children with adults')
+    )
+
+    target_audience= models.CharField(max_length=2, choices = TYPE_CHOICES)
+    workshop_duration = models.IntegerField(verbose_name="Workshop duration (min)")
+    workshop_price = models.DecimalField(max_digits = 6, decimal_places=2, verbose_name ="Cost pp (£)")
+
+    class Meta:
+        ordering = ["target_audience"]
+    
+    def __str__(self):
+        return f"{self.workshop_duration} minute workshop for {self.target_audience}"
+
+class Workshop(models.Model):
+    """ Stores a single workshop entry related to :model:`WorkshopCategory`"""
+
+    STATUS = ( (0, 'Draft'), (1, 'Open'), (2, 'Cancelled'), (3, 'Closed'))
+
+    category = models.ForeignKey(WorkshopType, on_delete=models.CASCADE, related_name= 'category')
+    session_name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    event_date = models.DateTimeField(unique=True)
+    location = models.CharField(max_length=400, default='on site')
+    excerpt = models.TextField(max_length= 500)
+    full_description = models.TextField()
+    max_places = models.IntegerField(default = 12)
+    primary_photo = CloudinaryField('cover_image', default ='placeholder_workshop' )
+    secondary_photo = CloudinaryField('extra_image', blank = True)
+    updated_on = models.DateField(auto_now= True)
+    publication_status = models.IntegerField(choices = STATUS, default = 0)
+
+    class Meta:
+        ordering = ['-event_date']
+
+    def __str__(self):
+        return f"{self.event_date} workshop for {self.category}"
